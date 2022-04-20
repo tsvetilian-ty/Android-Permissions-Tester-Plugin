53ce224c703b73d49520679e41c307f1572f9f36
Tsvetilian Yankov <tsvetilian.ty@gmail.com>
2022-04-20 19:48:42 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.5.10"
+    id("org.jetbrains.intellij") version "1.4.0"
+}
+
+group = "com.github.tsvetilian.ty"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+intellij {
+    version.set("211.7628.21")
+    type.set("IC")
+    plugins.set(listOf("android"))
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("211")
+        untilBuild.set("222.*")
+    }
+
+    runIde {
+        ideDir.set(file(System.getenv("ANDROID_IDE_PATH")))
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
