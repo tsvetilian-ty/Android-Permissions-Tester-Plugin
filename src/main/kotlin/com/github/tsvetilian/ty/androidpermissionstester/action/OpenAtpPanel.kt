24163db6398ab96c50dc7d3a57947a95571ee219
Tsvetilian Yankov <tsvetilian.ty@gmail.com>
2022-04-20 21:32:27 +0300

    Add as menu item in tools menu

---
+++ src/main/kotlin/com/github/tsvetilian/ty/androidpermissionstester/action/OpenAtpPanel.kt
@@ -0,0 +1,12 @@
+package com.github.tsvetilian.ty.androidpermissionstester.action
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class OpenAtpPanel : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        println("Action clicked")
+    }
+
+}
